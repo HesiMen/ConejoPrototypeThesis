d65af39a5d0a010dfeb679170a299c3bd27efd40
JonMoral <jonmoralesdesign@gmail.com>
2020-07-12 16:02:12 -0700

    Add Assets for Coyote/Fox
    -There may be a required dependency for AI later, if that's the case let me know and I can send that as a pacakge

---
+++ RabbitCoyote/Assets/Malbers Animations/Common/Behaviors/AnimalFollowController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.AI;
+
+using MalbersAnimations.Events;
+using MalbersAnimations.Utilities;
+using MalbersAnimations;
+
+public class AnimalFollowController : MonoBehaviour {
+
+	public AnimalType animal;
+	public float CollisionRadius;
+	public GameObject[] WayPoints;
+
+	//[HideInInspector]
+	public AnimalAIControl aiController;
+
+	protected bool following;
+	public bool traveling;
+	protected int wayPointCount = 0;
+	protected GameObject currentWaypoint;
+
+	// Use this for initialization
+	void Start () {
+		aiController = GetComponent<AnimalAIControl>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		UpdateAnimal ();
+	}
+
+	protected void UpdateAnimal()
+	{
+		if (!traveling)
+			CheckForPlayer ();
+		else
+		{
+			if (currentWaypoint != null)
+			{
+				if (Vector3.Distance (transform.position, currentWaypoint.transform.position) < aiController.stoppingDistance + 2f)
+				{
+					Debug.Log ("Reached " + currentWaypoint.name);
+					traveling = false;
+					currentWaypoint = null;
+				}
+			}
+		}
+	}
+
+	protected void FinishTask()
+	{
+	}
+
+	protected void CheckForPlayer()
+	{
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, CollisionRadius);
+
+		if (hitColliders.Length != 0)
+		{
+			
+		}
+		else
+		{
+			Debug.Log("No collisions!");
+		}
+
+		//following = false;
+		//aiController.SetTarget(null);
+
+	}
+
+	public string getWaypoint()
+	{
+		return WayPoints[(wayPointCount == 0 ? WayPoints.Length-1 : wayPointCount -1)].GetComponent<Waypoint>().AnimationTag;
+	}
+
+}
---
+++ RabbitCoyote/Assets/Malbers Animations/Common/Behaviors/Waypoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+	public string AnimationTag = "Default";
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<MeshRenderer>().enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
