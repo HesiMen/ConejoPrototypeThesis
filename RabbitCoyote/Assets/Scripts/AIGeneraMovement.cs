c876257f5a2da87da0dfb1bb69ce09c30d792efd
HesiMen <hesi.mendez@gmail.com>
2020-07-12 22:53:37 -0700

    umpgraded some mats

---
+++ RabbitCoyote/Assets/Scripts/AIGeneraMovement.cs
@@ -0,0 +1,165 @@
+﻿using MalbersAnimations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIGeneraMovement : MonoBehaviour
+{
+    #region References
+    public NavMeshAgent agent;
+    Animal animal;
+    public bool AutoSpeed = true;
+    public float seconds = 4f;
+    #endregion
+    public float stoppingDistance = 0.6f;
+    public Vector3 target;
+    public float wanderRadious;
+
+    public bool autoSpeed = true;
+    public float ToTrot = 6f;
+    public float ToRun = 8f;
+
+
+
+    public bool debug = false;                //Debuging 
+
+
+
+    public bool isMoving = false;
+
+
+    public NavMeshAgent Agent
+    {
+        get
+        {
+            if (agent == null)
+            {
+                agent = GetComponentInChildren<NavMeshAgent>();
+            }
+            return agent;
+        }
+    }
+
+    void Start()
+    {
+       // if (autoSpeed) AutomaticSpeed();
+
+        animal = GetComponent<Animal>();
+
+        target = RandomNavmeshLocation(wanderRadious);
+
+        agent.stoppingDistance = stoppingDistance;
+        //StartCoroutine(StartChasing());
+        AssignMovementTask(target);
+        animal.Move(target);
+    }
+
+    private void Update()
+    {
+
+
+       /*
+        Vector3 Direction = Vector3.zero;
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            Direction = agent.desiredVelocity.normalized;
+            isMoving = true;
+
+        }
+        else
+        {
+            animal.Action = true;
+            StartCoroutine(StartChasing());
+        }
+        */
+
+        
+        if (HasReachedPos())
+        {
+            animal.Action = true;
+            StartCoroutine(StartChasing());
+        }
+        
+
+
+    }
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+
+        return finalPosition;
+    }
+
+
+    public void AssignMovementTask(Vector3 goalPos)
+    {
+
+        agent.SetDestination(goalPos);
+
+    }
+
+    public bool HasReachedPos()
+    {
+        if (!agent.isOnNavMesh)
+            return false;
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!Agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    //Finished moving  do another target;
+                    return true;
+
+                }
+            }
+        }
+        return false;
+    }
+
+    IEnumerator StartChasing()
+    {
+        Agent.isStopped = true;
+        yield return new WaitForSeconds(seconds);
+        target = RandomNavmeshLocation(wanderRadious);
+        Agent.isStopped = false;
+        AssignMovementTask(target);
+        animal.Move(target);
+    }
+
+    public virtual void AutomaticSpeed()
+    {
+
+        if (Agent.remainingDistance < ToTrot)         //Set to Walk
+        {
+            animal.Speed1 = true;
+        }
+        else if (Agent.remainingDistance < ToRun)     //Set to Trot
+        {
+            animal.Speed2 = true;
+        }
+        else if (Agent.remainingDistance > ToRun)     //Set to Run
+        {
+            animal.Speed3 = true;
+        }
+    }
+    void OnDrawGizmos()
+    {
+        if (debug)
+        {
+           
+                Debug.DrawLine(transform.position, target, Color.green);
+            
+        }
+    }
+
+   
+}
