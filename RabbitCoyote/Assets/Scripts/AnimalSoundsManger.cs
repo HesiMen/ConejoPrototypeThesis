7082600ea446a273f277f5c08421819e2254b254
JonMoral <jonmoralesdesign@gmail.com>
2020-07-15 08:53:41 -0700

    Add Audio and Audio Manager for Coyote
    - Adding files to FMOD for Coyote SFX
    - AnimalSoundsManager.cs is the script needed to get the audio (attach to a GameObject)
    - To test audio, after you've set the 3 events and paths, you can set which key you would like to use to test your audio

---
+++ RabbitCoyote/Assets/Scripts/AnimalSoundsManger.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalSoundsManger : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public List<string> bankPath;
+
+    FMOD.Studio.EventInstance soundEvent1;
+    FMOD.Studio.EventInstance soundEvent2;
+    FMOD.Studio.EventInstance soundEvent3;
+
+    public KeyCode[] pressForSound;
+
+    [Range(1, 40)]
+    public int timeTillNextSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Create Instances for sound events
+        soundEvent1 = FMODUnity.RuntimeManager.CreateInstance(bankPath[0]);
+        soundEvent2 = FMODUnity.RuntimeManager.CreateInstance(bankPath[1]);
+        soundEvent3 = FMODUnity.RuntimeManager.CreateInstance(bankPath[2]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Attach to a game object
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent1, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent2, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent3, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        DebugPlaySound();
+
+        //Breathing Sounds
+        if ((Mathf.RoundToInt(Time.time) + 1) % timeTillNextSound == 0)
+        {
+            StartCoroutine(PlaySoundOverTime());
+        }
+
+    }
+
+    void DebugPlaySound()
+    {
+        if (Input.GetKeyDown(pressForSound[0]))
+        {
+            //Barking
+            soundEvent1.start();
+            Debug.Log("Played 1");
+        }
+        if (Input.GetKeyDown(pressForSound[1]))
+        {
+            //breathing
+            soundEvent2.start();
+            Debug.Log("Played 2");
+        }
+        if (Input.GetKeyDown(pressForSound[2]))
+        {
+            //Whining
+            soundEvent3.start();
+            Debug.Log("Played 3");
+        }
+    }
+
+    IEnumerator PlaySoundOverTime()
+    {
+        soundEvent2.start();
+        yield return new WaitForSeconds(5f);
+    }
+}
