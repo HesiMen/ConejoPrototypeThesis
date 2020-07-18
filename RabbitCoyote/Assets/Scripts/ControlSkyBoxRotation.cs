0609acd070c1a8311e221b51287744832f965b33
JonMoral <jonmoralesdesign@gmail.com>
2020-07-18 16:06:07 -0700

    Add 2D Environment & Scene
    - Adds Main 2D Scene where everything will be put

---
+++ RabbitCoyote/Assets/Scripts/ControlSkyBoxRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSkyBoxRotation : MonoBehaviour
+{
+    public Material skybox;
+    private float rotation;
+    public float rotationSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        skybox = RenderSettings.skybox;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateSkyBox();
+    }
+
+    public void RotateSkyBox()
+    {
+        rotation += Time.deltaTime * rotationSpeed;
+        skybox.SetFloat("_Rotation", rotation);
+    }
+}
