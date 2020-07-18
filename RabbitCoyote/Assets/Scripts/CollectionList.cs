46910d6e37688bdc916fcb5056b87f19e5a05eb6
JonMoral <jonmoralesdesign@gmail.com>
2020-07-18 00:05:15 -0700

    Add 2D Collection of Collectibles
    ****NOT CONNECTED TO UI SYSTEM*****
    
    - The system can be applied to VR just need to add the CollectibleManager script to the "bunny" and add the LerpToPlayer script to the desired collectible.
    
    -LerpToPlayer requires a target and therefore the lerp target can be any object (I should change the name :P) after lerping the object is then deactivated not destroyed
    
    - CollectibleManager makes the lerpState bool in LerpToPlayer true which then triggers the Lerp2Player function inside the script.

---
+++ RabbitCoyote/Assets/Scripts/CollectionList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionList : MonoBehaviour
+{
+    //Collection of Collectible objects
+    public List<GameObject> collectibles;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform child in transform)
+        {
+            collectibles.Add(child.gameObject);
+        }
+        
+    }
+}
