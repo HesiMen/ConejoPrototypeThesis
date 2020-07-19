0bae67440557fad1d7c37e985ddbcf8aac08a45f
HesiMen <hesi.mendez@gmail.com>
2020-07-18 18:20:51 -0700

    pushing
    movement plus ai

---
+++ RabbitCoyote/Assets/Scripts/TimeFreeze.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeFreeze : MonoBehaviour
+{
+  public void StopTime()
+    {
+        Time.timeScale = 0f;
+        Time.fixedDeltaTime = 0f;
+    }
+
+    public void ContinueTime()
+    {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+
+    }
+}
