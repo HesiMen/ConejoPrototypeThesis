88822caa41ef5cc6df3c7bb29189122920fa2c91
HesiMen <hesi.mendez@gmail.com>
2020-07-26 20:49:01 -0700

    Script for seperating

---
+++ RabbitCoyote/Assets/Scripts/MagueyScripts/SeperateAgaveScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class SeperateAgaveScript : MonoBehaviour
+{
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnGrab()
+    {
+        rb.isKinematic = false;
+        rb.useGravity = true;
+    }
+
+}
