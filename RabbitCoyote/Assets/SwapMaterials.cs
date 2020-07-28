f2be4500bff46e5a1adbdca9b1d9299765130e7e
JonMoral <jonmoralesdesign@gmail.com>
2020-07-27 23:15:48 -0700

    Add Material Swap for Maguey
    - Swaps betweeen materials that are defined in parent object, we can add more materials whenever we see fit and we we are able to swap materials whenever we want by calling the function in the SwapMaterials.cs script

---
+++ RabbitCoyote/Assets/SwapMaterials.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapMaterials : MonoBehaviour
+{
+    public List<Material> materials;
+    public int materialSwapIndex;
+
+    private Renderer[] childrenRend;
+
+    private void Start() 
+    {
+        childrenRend = GetComponentsInChildren<Renderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MaterialSwap();
+    }
+
+    public void MaterialSwap()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (materialSwapIndex >= materials.Count - 1)
+            {
+                materialSwapIndex = 0;
+            }
+            else
+            {
+                materialSwapIndex++;
+            }
+            
+            foreach(Renderer rend in childrenRend)
+            {
+                rend.material = materials[materialSwapIndex];
+            }
+        }
+    }
+}
