ed2c11ef1e10d1f7b5ce489b1595ac5911f1cccc
JonMoral <jonmoralesdesign@gmail.com>
2020-09-04 12:35:27 -0700

    Add Basic Rain and Day/Night
    - Adds Rain System with Clouds and ripples (not fully functional with code yet)
    - Adds Day Night Cycle that is partly functional with code but not fully completed

---
+++ RabbitCoyote/Assets/TargetFrameRate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFrameRate : MonoBehaviour
+{
+    public int frameRate = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = frameRate;
+    }
+
+}
