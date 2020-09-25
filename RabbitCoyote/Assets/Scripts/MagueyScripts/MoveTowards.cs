7b71c5934231028f69f6d46076269c3d8050cb80
HesiMen <hesi.mendez@gmail.com>
2020-09-25 10:55:14 -0700

    lots of metas sorry. was trying to build

--- RabbitCoyote/Assets/Scripts/MagueyScripts/MoveTowards.cs
+++ RabbitCoyote/Assets/Scripts/MagueyScripts/MoveTowards.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using UnityEditor.ShaderGraph.Internal;
+
 using UnityEngine.AI;
 
 public class MoveTowards : MonoBehaviour
