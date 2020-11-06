a4f9dfa310920d52e19817d2b7f13c63bea59c80
HesiMen <hesi.mendez@gmail.com>
2020-11-06 11:56:19 -0800

    AI And Eating WIP

--- RabbitCoyote/Assets/Scripts/MagueyScripts/AgaveObject.cs
+++ RabbitCoyote/Assets/Scripts/MagueyScripts/AgaveObject.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class AgaveObject : MonoBehaviour
 {
-   public enum AgaveObjectsInteractables { Sticks,FireStick , SmallRock, BigRock, Seed, AgaveLeaf, None}
+    public enum AgaveObjectsInteractables { Sticks, FireStick, SmallRock, BigRock, Seed, AgaveLeaf, None }
 
     public AgaveObjectsInteractables agaveObject = AgaveObjectsInteractables.None;
+
+    public bool _isEdible = false;
 }
